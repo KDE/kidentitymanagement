9bd0340e2acfbb0b11d6252b0c2025794d1c833b
Marc Mutz <mutz@kde.org>
2002-08-14 19:43:51 +0000

    GPLv2.0
    svn path=/trunk/kdenetwork/libkdenetwork/kmime_charfreq.cpp; revision=172031

--- kmime/kmime_charfreq.cpp
+++ kmime/kmime_charfreq.cpp
@@ -2,13 +2,12 @@
     kmime_charfreq.cpp
 
     KMime, the KDE internet mail/usenet news message library.
-    Copyright (c) 2001 the KMime authors.
+    Copyright (c) 2001-2002 the KMime authors.
     See file AUTHORS for details
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+    the Free Software Foundation; version 2 of the License.
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software Foundation,
     Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US
