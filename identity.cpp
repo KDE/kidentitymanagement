c1ea3e0dc08dda47ac29777ca1e2540ccbd018a8
Tom Albers <toma@kde.org>
2007-06-26 17:30:00 +0000

    Relicense libkpimidentites, approved by Marc Mutz (main copyright holder) in a mail to Till, as announced on the kde-pim mailinglist.
    svn path=/branches/work/libkpimidentities/; revision=680609

--- identity.cpp
+++ identity.cpp
@@ -1,6 +1,21 @@
-// -*- mode: C++; c-file-style: "gnu" -*-
-// kmidentity.cpp
-// License: GPL
+/*
+    Copyright (c) 2002-2004 Marc Mutz <mutz@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
 
 #include "identity.h"
 
--- identity.h
+++ identity.h
@@ -1,9 +1,23 @@
-/* -*- mode: C++; c-file-style: "gnu" -*-
- * User identity information
- *
- * Author: Stefan Taferner <taferner@kde.org>
- * This code is under GPL
- */
+/*
+    Copyright (c) 2002-2004 Marc Mutz <mutz@kde.org>
+    Author: Stefan Taferner <taferner@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
 #ifndef kpim_identity_h
 #define kpim_identity_h
 
