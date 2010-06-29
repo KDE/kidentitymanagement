da4dca385ed124e1d9bea304d17fa8317b016420
Thomas McGuire <mcguire@kde.org>
2010-06-29 18:49:27 +0000

    Fix and extend the identity test a bit.
    svn path=/trunk/KDE/kdepimlibs/; revision=1144365

--- tests/identitytest.cpp
+++ tests/identitytest.cpp
@@ -34,10 +34,30 @@ void IdentityTester::test_NullIdentity()
 void IdentityTester::test_Aliases()
 {
   IdentityManager manager;
-  Identity i1 = manager.newFromScratch( "Test" );
+
+  // It is picking up identities from older tests somethimes, so cleanup
+  while ( manager.identities().size() > 1 ) {
+    manager.removeIdentity( manager.identities().first() );
+    manager.commit();
+  }
+
+  Identity &i1 = manager.newFromScratch( "Test1" );
   i1.setPrimaryEmailAddress( "firstname.lastname@example.com" );
   i1.setEmailAliases( QStringList() << "firstname@example.com" << "lastname@example.com" );
   QVERIFY( i1.matchesEmailAddress( "\"Lastname, Firstname\" <firstname@example.com>" ) );
   QVERIFY( i1.matchesEmailAddress( "\"Lastname, Firstname\" <firstname.lastname@example.com>" ) );
   QCOMPARE( i1.emailAliases().size(), 2 );
+  manager.commit();
+
+  Identity &i2 = manager.newFromScratch( "Test2" );
+  i2.setPrimaryEmailAddress( "test@test.de" );
+  QVERIFY( i2.emailAliases().isEmpty() );
+  manager.commit();
+
+  // Remove the first identity, which we couldn't remove above
+  manager.removeIdentity( manager.identities().first() );
+  manager.commit();
+
+  QCOMPARE( manager.allEmails().size(), 4 );
+  QCOMPARE( manager.identityForAddress( "firstname@example.com" ).identityName().toAscii().data(), "Test1" );
 }
