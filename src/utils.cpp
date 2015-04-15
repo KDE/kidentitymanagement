ad5b623efdee7fcd4fc31fbcca47e55be82665fc
Christian Mollekopf <chrigi_1@fastmail.fm>
2015-04-15 14:17:01 +0200

    Added utils for cached version for thatIsMe and allEmails
    Conflicts:
    	kpimidentities/CMakeLists.txt

---
+++ src/utils.cpp
@@ -0,0 +1,69 @@
+/*
+  Copyright (c) 2014 Sandro Knauß <knauss@kolabsys.com>
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or ( at your
+  option ) any later version.
+
+  This library is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+  License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+*/
+
+#include "utils.h"
+#include "identitymanager.h"
+
+#include <QObject>
+
+using namespace KIdentityManagement;
+
+class IdendentitiesCache : public QObject
+{
+    Q_OBJECT
+public:
+    IdendentitiesCache()
+        : mIdentityManager(/*ro=*/ true)
+    {
+        connect(&mIdentityManager, SIGNAL(changed()),
+                SLOT(slotIdentitiesChanged()));
+        slotIdentitiesChanged();
+    }
+    const QSet<QString> &emails()
+    {
+        return mEmails;
+    }
+
+private Q_SLOTS:
+    void slotIdentitiesChanged()
+    {
+        mEmails.clear();
+        foreach (const QString &email, mIdentityManager.allEmails()) {
+            mEmails.insert(email.toLower());
+        }
+    }
+
+private:
+    IdentityManager mIdentityManager;
+    QSet<QString> mEmails;
+};
+
+Q_GLOBAL_STATIC(IdendentitiesCache, sIdentitiesCache)
+
+bool KIdentityManagement::thatIsMe(const QString &email)
+{
+    return allEmails().contains(email.toLower());
+}
+
+const QSet<QString> &KIdentityManagement::allEmails()
+{
+    return sIdentitiesCache()->emails();
+}
+
+#include "utils.moc"
---
+++ src/utils.h
@@ -0,0 +1,45 @@
+/*
+  Copyright (c) 2014 Sandro Knauß <knauss@kolabsys.com>
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or ( at your
+  option ) any later version.
+
+  This library is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+  License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+*/
+
+#ifndef KPIMIDENTITES_UTILS_H
+#define KPIMIDENTITES_UTILS_H
+
+#include "kidentitymanagement_export.h"
+
+#include <QString>
+#include <QSet>
+
+namespace KIdentityManagement
+{
+    /*
+    * Very fast version of IdentityManager::thatIsMe, that is using an internal cache (allEmails)
+    * - make sure that only an email address is used as parameter and NO name <email>
+    * - emails are tested with email.toLower(), so no need to lower them before.
+    */
+    KIDENTITYMANAGEMENT_EXPORT bool thatIsMe(const QString &email);
+
+    /*
+    * Very fast version of IdentityManager::allEmails , that is using an internal cache.
+    * The cache is updated with IdentityManager::changed signal.
+    * All email addresses + alias of the identities. The email addresses are all lowered.
+    */
+    KIDENTITYMANAGEMENT_EXPORT const QSet<QString> &allEmails();
+}
+
+#endif
