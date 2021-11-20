2eefc943d98ab19114893bde3c87513229f6e189
Laurent Montel <montel@kde.org>
2021-11-20 17:34:45 +0100

    GIT_SILENT: add missing Q_REQUIRED_RESULT

--- src/utils.cpp
+++ src/utils.cpp
@@ -23,7 +23,7 @@ public:
         slotIdentitiesChanged();
     }
 
-    const QSet<QString> &emails() const
+    Q_REQUIRED_RESULT const QSet<QString> &emails() const
     {
         return mEmails;
     }
