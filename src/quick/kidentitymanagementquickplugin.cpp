0cb4ffecd0609fe62c78960633823a1188c8fcdf
Claudio Cambra <claudio.cambra@kde.org>
2023-08-24 17:58:23 +0800

    Add starter quick plugin
    Signed-off-by: Claudio Cambra <claudio.cambra@kde.org>

---
+++ src/quick/kidentitymanagementquickplugin.cpp
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2023 Claudio Cambra <claudio.cambra@kde.org>
+// SPDX-License-Identifier: LGPL-2.0-or-later
+
+#include "kidentitymanagementquickplugin.h"
+
+void KIdentityManagementQuickPlugin::registerTypes(const char *uri)
+{
+    Q_UNUSED(uri);
+}
