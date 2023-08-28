dc4ae25fc4c86a03c089dd62790a8bf4f64ec26c
Claudio Cambra <claudio.cambra@kde.org>
2023-08-28 13:22:44 +0800

    Change KeyListModel namespace into an Interface class
    Signed-off-by: Claudio Cambra <claudio.cambra@kde.org>

---
+++ src/quick/keylistmodelinterface.h
@@ -0,0 +1,37 @@
+// SPDX-FileCopyrightText: 2023 Claudio Cambra <claudio.cambra@kde.org>
+// SPDX-License-Identifier: LGPL-2.0-or-later
+
+#pragma once
+
+#include "kidentitymanagementquick_export.h"
+
+#include <QHash>
+#include <QObject>
+
+class QModelIndex;
+
+namespace KIdentityManagement
+{
+
+namespace Quick
+{
+
+/**
+ * @brief This interface defines the roles and rolenames that are expected
+ * in the QtQuick Cryptography editor.
+ */
+class KIDENTITYMANAGEMENTQUICK_EXPORT KeyListModelInterface
+{
+public:
+    virtual ~KeyListModelInterface(){};
+    enum Roles { KeyIdentifierRole = Qt::UserRole + 1, KeyByteArrayRole };
+
+    static QHash<int, QByteArray> roleNames()
+    {
+        return {{KeyIdentifierRole, "keyIdentifier"}, {KeyByteArrayRole, "keyByteArray"}};
+    }
+};
+}
+}
+
+Q_DECLARE_INTERFACE(KIdentityManagement::Quick::KeyListModelInterface, "KeyListModelInterface")
