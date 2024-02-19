089a0a9031adf1118bc148f3711c01f73af2e362
Laurent Montel <montel@kde.org>
2024-02-19 13:51:20 +0100

    Missing -o => we didn't extract string from qml

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 # SPDX-License-Identifier: BSD-3-Clause
 # SPDX-FileCopyrightText: none
-$XGETTEXT `find -name *.cpp -o -name *.h -name \*.qml` -o $podir/libkpimidentities6.pot
+$XGETTEXT `find -name *.cpp -o -name *.h -o -name \*.qml` -o $podir/libkpimidentities6.pot
