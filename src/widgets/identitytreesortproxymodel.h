a66c82c9e11a26662dffab73a35021760bf5185f
Laurent Montel <montel@kde.org>
2024-04-07 09:20:41 +0200

    Add sortproxymodel

---
+++ src/widgets/identitytreesortproxymodel.h
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+
+#pragma once
+#include "kidentitymanagementwidgets_export.h"
+
+#include <QSortFilterProxyModel>
+
+namespace KIdentityManagementWidgets
+{
+class KIDENTITYMANAGEMENTWIDGETS_EXPORT IdentityTreeSortProxyModel : public QSortFilterProxyModel
+{
+public:
+    explicit IdentityTreeSortProxyModel(QObject *parent);
+    ~IdentityTreeSortProxyModel() override;
+};
+}
