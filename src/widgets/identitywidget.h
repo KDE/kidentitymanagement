66b91564b3f14897c638f3437426147b6f2cebf7
Laurent Montel <montel@kde.org>
2024-04-07 09:38:39 +0200

    Prepare to add autotests

--- src/widgets/identitywidget.h
+++ src/widgets/identitywidget.h
@@ -2,10 +2,11 @@
 // SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 #pragma once
 
+#include "kidentitymanagementwidgets_export.h"
 #include <QWidget>
 namespace KIdentityManagementWidgets
 {
-class IdentityWidget : public QWidget
+class KIDENTITYMANAGEMENTWIDGETS_EXPORT IdentityWidget : public QWidget
 {
     Q_OBJECT
 public:
