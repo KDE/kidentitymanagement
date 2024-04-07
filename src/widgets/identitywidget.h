565a22448eca21ce0614aa272296d6cb0748244f
Laurent Montel <montel@kde.org>
2024-04-07 09:36:31 +0200

    Add identitywidget

---
+++ src/widgets/identitywidget.h
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+#pragma once
+
+#include <QWidget>
+namespace KIdentityManagementWidgets
+{
+class IdentityWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit IdentityWidget(QWidget *parent = nullptr);
+    ~IdentityWidget() override;
+};
+
+}
