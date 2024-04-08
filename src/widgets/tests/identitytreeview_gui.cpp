20357b01fe55f8e115d53137a856d5924cac0e50
Laurent Montel <montel@kde.org>
2024-04-08 08:18:19 +0200

    Add test apps

---
+++ src/widgets/tests/identitytreeview_gui.cpp
@@ -0,0 +1,32 @@
+// SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+// SPDX-License-Identifier: LGPL-2.0-or-later
+
+#include <KIdentityManagementCore/IdentityManager>
+#include <KIdentityManagementWidgets/IdentityTreeView>
+#include <QApplication>
+#include <QVBoxLayout>
+#include <QWidget>
+
+class IdentityTreeWidget : public QWidget
+{
+public:
+    explicit IdentityTreeWidget(QWidget *parent = nullptr);
+    ~IdentityTreeWidget() override = default;
+};
+
+IdentityTreeWidget::IdentityTreeWidget(QWidget *parent)
+    : QWidget(parent)
+{
+    auto mainLayout = new QVBoxLayout(this);
+    auto treeview = new KIdentityManagementWidgets::IdentityTreeView(this);
+    mainLayout->addWidget(treeview);
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    auto w = new IdentityTreeWidget;
+    w->show();
+    return app.exec();
+}
